e4b64c2ad4fbc4765ed6ca1286f19ca79f5c8e49
chinadupaya <annysiad@yahoo.com>
2024-11-11 22:39:34 +0100

    Update graph and page to compare with v1

--- docs/air-quality/index.md
+++ docs/air-quality/index.md
@@ -6,6 +6,9 @@
 
 ![Forecast](./assets/img/pm25_forecast.png)
 
+## V1 Comparison
+![Forecast1](./assets/img/pm25_forecast_1.png)
+
 
 There is also a Python program to interact with the air quality ML system using language (text, voice),
 powered by a [function-calling LLM](https://www.hopsworks.ai/dictionary/function-calling-with-llms).
@@ -15,3 +18,6 @@ powered by a [function-calling LLM](https://www.hopsworks.ai/dictionary/function
 1-Day Hindcast: Predictions vs Outcomes
 
 ![Hindcast](./assets/img/pm25_hindcast_1day.png)
+
+## V1 Comparison
+![Hindcast1](./assets/img/pm25_hindcast_1day_1.png)
--- notebooks/ch03/functions/util.py
+++ notebooks/ch03/functions/util.py
@@ -183,7 +183,55 @@ def get_pm25(aqicn_url: str, country: str, city: str, street: str, day: datetime
 
     return aq_today_df
 
+def plot_air_quality_forecast_merge(city: str, street: str, df: pd.DataFrame, df_1: pd.DataFrame, file_path: str, hindcast=False):
+    fig, ax = plt.subplots(figsize=(10, 6))
 
+    day = pd.to_datetime(df['date']).dt.date
+    day_1 = pd.to_datetime(df_1['date']).dt.date
+    # Plot each column separately in matplotlib
+    ax.plot(day, df['predicted_pm25'], label='Predicted PM2.5', color='red', linewidth=2, marker='o', markersize=5, markerfacecolor='blue')
+    ax.plot(day_1, df_1['predicted_pm25'], label='Predicted PM2.5 v1', color='blue', linewidth=2, marker='o', markersize=5, markerfacecolor='blue')
+
+    # Set the y-axis to a logarithmic scale
+    ax.set_yscale('log')
+    ax.set_yticks([0, 10, 25, 50, 100, 250, 500])
+    ax.get_yaxis().set_major_formatter(plt.ScalarFormatter())
+    ax.set_ylim(bottom=1)
+
+    # Set the labels and title
+    ax.set_xlabel('Date')
+    ax.set_title(f"PM2.5 Predicted (Logarithmic Scale) for {city}, {street}")
+    ax.set_ylabel('PM2.5')
+
+    colors = ['green', 'yellow', 'orange', 'red', 'purple', 'darkred']
+    labels = ['Good', 'Moderate', 'Unhealthy for Some', 'Unhealthy', 'Very Unhealthy', 'Hazardous']
+    ranges = [(0, 49), (50, 99), (100, 149), (150, 199), (200, 299), (300, 500)]
+    for color, (start, end) in zip(colors, ranges):
+        ax.axhspan(start, end, color=color, alpha=0.3)
+
+    # Add a legend for the different Air Quality Categories
+    patches = [Patch(color=colors[i], label=f"{labels[i]}: {ranges[i][0]}-{ranges[i][1]}") for i in range(len(colors))]
+    legend1 = ax.legend(handles=patches, loc='upper right', title="Air Quality Categories", fontsize='x-small')
+
+    # Aim for ~10 annotated values on x-axis, will work for both forecasts ans hindcasts
+    if len(df.index) > 11:
+        every_x_tick = len(df.index) / 10
+        ax.xaxis.set_major_locator(MultipleLocator(every_x_tick))
+
+    plt.xticks(rotation=45)
+
+    if hindcast == True:
+        ax.set_xlim([datetime.today() - timedelta(weeks=1), datetime.today() + timedelta(weeks=1)])
+        ax.plot(day, df['pm25'], label='Actual PM2.5', color='black', linewidth=2, marker='^', markersize=5, markerfacecolor='grey')
+        legend2 = ax.legend(loc='upper left', fontsize='x-small')
+        ax.add_artist(legend1)
+
+    # Ensure everything is laid out neatly
+    plt.tight_layout()
+
+    # # Save the figure, overwriting any existing file with the same name
+    plt.savefig(file_path)
+    return plt
 def plot_air_quality_forecast(city: str, street: str, df: pd.DataFrame, file_path: str, hindcast=False):
     fig, ax = plt.subplots(figsize=(10, 6))
 
